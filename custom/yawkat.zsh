1416f5817427bf35aa9383aff787b6b47056569a
Jonas Konrad <me@yawk.at>
2014-04-11 15:41:00 +0200

    Customization

---
+++ custom/yawkat.zsh
@@ -0,0 +1,18 @@
+ZSH_THEME=""
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_no_bold[cyan]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX=" "
+ZSH_THEME_GIT_PROMPT_DIRTY="*"
+ZSH_THEME_GIT_PROMPT_CLEAN=""
+
+PROMPT="%{$fg_bold[green]%}%~%{$fg_no_bold[green]%}%# %{$reset_color%}"
+RPROMPT="$(git_prompt_info)%{$fg_no_bold[green]%}%n@%m%{$reset_color%}"
+
+source "$ZSH/plugins/mvn/mvn.plugin.zsh"
+alias mvn="mvn-color"
+
+source "$ZSH/plugins/rsync/rsync.plugin.zsh"
+source "$ZSH/plugins/sudo/sudo.plugin.zsh"
+
+UPDATE_ZSH_DAYS=1
+DISABLE_AUTO_UPDATE="false"
